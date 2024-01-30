3c299511709fae804b9b4ca5f483aabc4d5b0c36
Lacamoura48 <anasfog@outlook.com>
2024-01-30 13:07:43 +0100

    gg

--- script.js
+++ script.js
@@ -1,47 +1,26 @@
-const fadeInImage = document.getElementById("transparent-grey-div");
-document.addEventListener("DOMContentLoaded", function() {
-  // Initial state
-  fadeInImage.style.opacity = 0;
-  fadeInImage.style.transform = "scale(0)";
-  setTimeout(function() {
-    // Final state
-    fadeInImage.style.opacity = 1;
-    fadeInImage.style.transform = "scale(1)";
-    fadeInImage.style.borderRadius = "0";
-  }, 500); // 3000 milliseconds (3 seconds) delay before the image enlarges and fades in
-});
 
-
-var myDiv = document.getElementById("myDiv");
-
-setTimeout(function() {
-  myDiv.style.transition = "background-color 1s ease";
-  myDiv.style.backgroundColor = "rgba(10, 10, 10, 0.5)";
-}, 2000);
-
- document.body.classList.add('show-image');
-
-//------------------------------------------------------------------------------Roulette
-
-let roulette = document.querySelector("#roulette");
-let wheel =  document.querySelector(".wheel");
-let shuriken = document.querySelector(".shuriken")
-let blurredWheel = document.querySelector(".blurred-wheel")
 let btn = document.getElementById("spin");
-let number = 3000;
 
+let number = 0;
 let clicks = 0;
-btn.onclick = function () {
+
+const getPrize = () =>
+  new Promise((resolve) => {
+    setTimeout(() => resolve(3000), 4000);
+  });
+
+btn.onclick = async function () {
   clicks += 1;
-	roulette.style.transform = "translate(-50%, -50%) rotate(" + number + "deg)";
-	shuriken.style.transform = "translate(-50%, -50%) rotate(" + number + "deg)";
-  setTimeout(()=>{
-    blurredWheel.style.opacity = 1
-    // wheel.style.display = 'none'
-  }, 1600)
-  setTimeout(()=>{
-    blurredWheel.style.opacity = 0
-    // wheel.style.display = 'block'
-  }, 3000)
-	number += 3000;
+  // initial rotation -------------------
+  gsap.fromTo(".wheel",{rotate: number}, {rotate: number + 1500, duration: 3, ease: "power3.in",})
+  gsap.to(".blurred-wheel", {opacity: 1, duration: 0.5, delay: 1.8})
+
+  // add where it lands
+  gsap.set(".wheel", {rotate: 0, delay: 3.2})
+  const prizeDegree = await getPrize();
+console.log(prizeDegree);
+  // reveal animation
+  number = prizeDegree;
+  gsap.to(".blurred-wheel", {opacity: 0, duration: 0.5})
+  gsap.fromTo(".wheel",{rotate: 0}, {rotate: number, duration: 12, ease: "power4.out"})
 }
