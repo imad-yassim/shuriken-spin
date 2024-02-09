9111b8c3c84b70a227cf93664249e6ae7eafd5d4
Lacamoura48 <anasfog@outlook.com>
2024-02-10 00:27:02 +0100

    hh

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ const getPrize = () =>
 btn.onclick = async function () {
   clicks += 1;
   // initial rotation -------------------
-  gsap.fromTo(".wheel",{rotate: number}, {rotate: number + 1500, duration: 3, ease: "power3.in",})
+  gsap.fromTo(".wheel",{rotate: number}, {rotate: number + 1500, duration: 3, ease: "power3.in"})
   gsap.to(".blurred-wheel", {opacity: 1, duration: 0.5, delay: 1.8})
 
   // add where it lands
@@ -24,3 +24,33 @@ console.log(prizeDegree);
   gsap.to(".blurred-wheel", {opacity: 0, duration: 0.5})
   gsap.fromTo(".wheel",{rotate: 0}, {rotate: number, duration: 12, ease: "power4.out"})
 }
+
+
+const rewardsBtn = document.getElementById("rewardsBtn");
+const nftsBtn = document.getElementById("nftsBtn");
+const closeTabButton = document.querySelectorAll(".closeBtn");
+
+
+function openTab (tab){
+  if(tab === 0){
+    gsap.to("#rewardsTab", {x: "-50%", ease: "ease.in"})
+  }else {
+    gsap.to("#nftsTab", {x: "-50%", ease: "ease.in"})
+  }
+  closeTabButton[0].style.display = "block"
+  closeTabButton[1].style.display = "block"
+}
+
+function closeTab (){
+    gsap.to("#rewardsTab", {x: "170%"})
+    gsap.to("#nftsTab", {x: "-170%"})
+
+    closeTabButton[0].style.display = "none"
+    closeTabButton[1].style.display = "none"
+}
+
+rewardsBtn.onclick = () => openTab(0);
+nftsBtn.onclick = () => openTab(1);
+
+closeTabButton[0].onclick = () => closeTab()
+closeTabButton[1].onclick = () => closeTab()
